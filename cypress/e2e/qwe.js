25d6140134b312d1223526e3000595d679c705c0
Dagudda98 <tanir.akhmetov.98@bk.ru>
2025-03-01 12:38:34 +0500

    add

--- cypress/e2e/qwe.js
+++ cypress/e2e/qwe.js
@@ -1,19 +1,15 @@
-describe('Login Test', () => {
-  beforeEach(() => {
-    cy.visit('https://www.saucedemo.com/') 
-
-    cy.log('Авторизация')
-    cy.get('[data-test="username"]').type('standard_user') 
-    cy.get('[data-test="password"]').type('secret_sauce') 
-    cy.get('[data-test="login-button"]').click() 
-  })
-  it('Проверяет, что ссылки ведут на правильные страницы', () => {
-    cy.wait(2000);
-    cy.get('[data-test="social-twitter"]').should('have.attr',  'href', 'https://twitter.com/saucelabs');
-    cy.wait(2000);
-    cy.get('[data-test="social-facebook"]').should('have.attr', 'href', 'https://www.facebook.com/saucelabs');
-    cy.wait(2000);
-    cy.get('[data-test="social-linkedin"]').should('have.attr', 'href', 'https://www.linkedin.com/company/sauce-labs/');
-    
+describe('API tests', () => {
+  it('Get-request', () => {
+    cy.request('GET', 'https://httpbin.org/get').then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('url', 'https://httpbin.org/get');
+    });
   });
-});
+
+  it('Post-request', () =>{
+    cy.request('POST', 'https://httpbin.org/post', {name: 'TestUser'}).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.json).to.deep.equal({name: 'TestUser'});
+    });
+  });
+})
