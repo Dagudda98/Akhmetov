09273af9f86f9f0791aa3488c56c226a72278437
Dagudda98 <tanir.akhmetov.98@bk.ru>
2026-01-26 22:59:41 +0500

    Remove Allure, use default Cypress reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,13 @@
 const { defineConfig } = require('cypress');
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      console.log('Setting up Allure plugin');
-      allureWriter(on, config);
+      // тут пока ничего не нужно
       return config;
     },
-    reporter: 'cypress-allure-plugin',
-    reporterOptions: {
-      resultsDir: 'C:/ProgramData/Jenkins/.jenkins/workspace/Cypress-test/allure-results' // Указан путь для Allure-отчётов
-    },
+    video: true,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos"
   },
 });
